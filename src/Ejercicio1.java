cce71e550c5b6b61006a134db745bd4dc5ac8d15
gladysalvarado <gyalvarado@hotmail.com>
2023-10-23 16:03:10 +0200

    Initial commit

--- src/Ejercicio1.java
+++ src/Ejercicio1.java
@@ -1,5 +1,19 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+public class Ejercicio1 {
+    public static void main(String[] args){
+        String nombre = "Manolo";
+        System.out.println(nombre.toLowerCase());
+        System.out.println(nombre.toUpperCase());
+        System.out.println(nombre);
+        System.out.println(nombre.isEmpty());
+        System.out.println(nombre.indexOf("n"));
+        System.out.println(nombre.indexOf("o"));
+        System.out.println(nombre.lastIndexOf("o"));
+        System.out.println(nombre.substring(4));
+        System.out.println(nombre.substring(2,4));
+        System.out.println(nombre.length());
+
+
+
+
+            }
 }
